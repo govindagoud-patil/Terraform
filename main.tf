cdc6c4c3828b935ff4a2e2b5719f824efc34953b
govindagoud <govindagoud.patil@hotmail.com>
2021-06-02 12:34:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,16 @@
 terraform {
-  required_version = ">= 0.12.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
 }
+
+provider "azurerm" {
+  features {}
+}
+
 
 resource "azurerm_resource_group" "resource_group" {
   name     = "${var.application}-${var.environment}"
