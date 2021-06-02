211122269e8e6c075e7ba20fe61a77bf225d4d05
govindagoud <govindagoud.patil@hotmail.com>
2021-06-02 17:04:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,14 +15,14 @@ provider "azurerm" {
 resource "azurerm_resource_group" "resource_group" {
   name     = "${var.application}-${var.environment}"
   location = "${var.location}"
-  tags     = "${merge(var.default_tags, tomap({"type", "resource"}))}"
+  tags     = "${merge(var.default_tags, tomap({type="resource"}))}"
 }
 
 module "application-vnet" {
   source              = "./modules/vnet"
   resource_group_name = "${azurerm_resource_group.resource_group.name}"
   location            = "${var.location}"
-  tags                = "${merge(var.default_tags, tomap({"type", "network"}))}"
+  tags                = "${merge(var.default_tags, tomap({type="network"}))}"
   vnet_name           = "${azurerm_resource_group.resource_group.name}-vnet"
   address_space       = "${var.address_space}"
 }
@@ -31,7 +31,7 @@ module "application-subnets" {
   source              = "./modules/subnet"
   resource_group_name = "${azurerm_resource_group.resource_group.name}"
   location            = "${var.location}"
-  tags                = "${merge(var.default_tags, tomap({"type", "network"}))}"
+  tags                = "${merge(var.default_tags, tomap({type="network"}))}"
   vnet_name           = "${module.application-vnet.vnet_name}"
 
   subnets = [
@@ -45,7 +45,7 @@ module "vmss" {
   source              = "./modules/vmss"
   resource_group_name = "${azurerm_resource_group.resource_group.name}"
   location            = "${var.location}"
-  tags                = "${merge(var.default_tags, tomap({"type", "vmss"}))}"
+  tags                = "${merge(var.default_tags, tomap({type="vmss"}))}"
   saname              = "${var.application}${var.environment}"
   capacity            = "${var.capacity}"
   subnet_id           = "${module.application-subnets.vnet_subnets}"
