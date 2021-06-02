df49ee988d3f44a6416729272a18b4b7d0137bba
govindagoud <govindagoud.patil@hotmail.com>
2021-06-02 12:30:40 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,4 +1,8 @@
 terraform {
   backend "azurerm" {
+    resource_group_name  = "keyvault1"
+    storage_account_name = "gtpkeyvaulttest"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
 }
